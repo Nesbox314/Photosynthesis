91e04130bbdc00cc816f276f540d55e8db37791f
leonardo-kons <49040747+leonardo-kons@users.noreply.github.com>
2020-03-15 10:53:56 -0300

    Update routes.js
    comit de teste

--- Photosynthesis/src/routes.js
+++ Photosynthesis/src/routes.js
@@ -1,6 +1,7 @@
 import { createStackNavigator } from 'react-navigation';
 
 import Login from './pages/login';
+import Homepage from './pages/homepage';
 
 export default createStackNavigator({
     Login
