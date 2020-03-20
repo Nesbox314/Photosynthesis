9880aa6f411c3b85962740216418d5faf3cced5f
Nesbox314 <ricardopop96@gmail.com>
2020-03-20 19:35:29 -0300

    API Criada, apenas restando colocar as rotas essenciais

--- PhotoAPI/routes/index.js
+++ PhotoAPI/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const app = express();
+const port = 3005; //porta padrão
+const mysql = require('mysql');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  port     :  3306,
+  user     : 'root',
+  password : '',
+  database : 'photoapi'
 });
+
+//inicia o servidor
+app.listen(port, '0.0.0.0');
+
+//pronto para colocação de rotas
 
 module.exports = router;
