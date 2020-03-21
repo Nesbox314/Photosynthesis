dc9a6dd2abd5a8d09381d0475d8c7735759224ff
Nesbox314 <ricardopop96@gmail.com>
2020-03-20 22:58:03 -0300

    Get criado para pegar o último registro da tabela umidade

--- PhotoAPI/routes/index.js
+++ PhotoAPI/routes/index.js
@@ -17,12 +17,12 @@ app.listen(port, '0.0.0.0');
 
 //exemplo de rota: localhost:3000/postUmidade?estadoUmidade=seco&estadoLuminosidade=bom
 router.post('/postUmidade', function(req, res, next) {
-  connection.query(`INSERT INTO umidade (estadoUmidade, data, estadoLuminosidade) VALUES ('${req.query.estadoUmidade}', '${Date.now()}', '${req.query.estadoLuminosidade}');`, function(err, results, fields) {
-    if(err){
-      console.log(err)
-      res.send('Falha na inserção de dados');
-    }
-  });
+    connection.query(`INSERT INTO umidade (estadoUmidade, data, estadoLuminosidade) VALUES ('${req.query.estadoUmidade}', '${Date.now()}', '${req.query.estadoLuminosidade}');`, function(err, results, fields) {
+        if(err){
+          console.log(err)
+          res.send('Falha na inserção de dados');
+        }
+    });
   res.send('Dados inseridos com sucesso');
 });
 
@@ -33,6 +33,16 @@ router.get('/getTodasUmidade', function(req, res, next) {
           res.send('Erro ao coletar dados');
         }
         res.send(results);
+    });
+});
+
+router.get('/getUltimaUmidade', function(req, res, next) {
+  connection.query("SELECT * FROM umidade WHERE data IN (SELECT MAX(data) FROM umidade)", function(err, results, fields) {
+        if(err){
+          console.log(err);
+          res.send('Erro ao coletar dados');
+        }
+        res.send(results)
     });
 });
 
