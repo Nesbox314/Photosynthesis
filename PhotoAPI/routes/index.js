9d30eaf33dcfddb26a9671ee145498e904750411
Nesbox314 <ricardopop96@gmail.com>
2020-03-20 21:23:26 -0300

    Criado post para postagem de dados na tabela umidade

--- PhotoAPI/routes/index.js
+++ PhotoAPI/routes/index.js
@@ -9,12 +9,21 @@ const connection = mysql.createConnection({
   port     :  3306,
   user     : 'root',
   password : '',
-  database : 'photoapi'
+  database : 'photosynthesis'
 });
 
 //inicia o servidor
 app.listen(port, '0.0.0.0');
 
-//pronto para colocação de rotas
+//exemplo de rota: localhost:3000/postData?estadoUmidade=seco&estadoLuminosidade=bom
+router.post('/postData', function(req, res, next) {
+  connection.query(`INSERT INTO umidade (estadoUmidade, data, estadoLuminosidade) VALUES ('${req.query.estadoUmidade}', '${Date.now()}', '${req.query.estadoLuminosidade}');`, function(err, results, fields) {
+    if(err){
+      console.log(err)
+      res.send('Falha na inserção de dados');
+    }
+  });
+  res.send('Dados inseridos com sucesso');
+});
 
 module.exports = router;
