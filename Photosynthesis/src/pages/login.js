0d03e7b7c8466148a3263810defd75767ad89ef0
Nesbox314 <ricardopop96@gmail.com>
2020-03-22 22:08:28 -0300

    Criado front-end da tela de login, restando aplicar métodos

--- Photosynthesis/src/pages/login.js
+++ Photosynthesis/src/pages/login.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, Button, Alert } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
-import { getOrientationAsync } from 'expo/build/ScreenOrientation/ScreenOrientation';
 
 export default class Login extends Component {
 
     render(){
         return (
-            <View>
-                <Image source={require('../../assets/logo.png')} style={styles.logo}></Image>
-                <TextInput style={styles.input}/>
+            <View style={{flex: 1,backgroundColor: 'white'}}>
+                <View>
+                    <Image source={require('../../assets/logo.png')} style={styles.logo}></Image>
+                </View>
+                <View style={styles.inputs}>
+                    <TextInput style={styles.input} placeholderTextColor={'rgb(100, 100, 100)'} placeholder={'\xa0' + "E-mail ou nome do usuário"}/>
+                    <TextInput style={styles.input} placeholderTextColor={'rgb(100, 100, 100)'} placeholder={'\xa0' + "Senha"}/>
+                </View>
+                <View style={styles.button}>
+                    <Button color={'rgb(146, 211, 110)'} title={"Entrar"} onPress={() => Alert.alert('Método para login')}/>
+                </View>
+                <View style={styles.button}>
+                    <Text style={styles.helpSenha}>Esqueceu seus dados de entrada? <Text style={styles.helpSenha, {fontWeight: "bold"}}>Obtenha ajuda para entrar.</Text></Text>
+                </View>
+                <View style={styles.footer}>
+                    <Text style={styles.helpSenha}>Não tem conta? <Text style={styles.helpSenha, {fontWeight: "bold"}}>Cadastre-se!</Text></Text>
+                </View>
             </View>
         )
     }
@@ -24,9 +37,25 @@ const styles = StyleSheet.create({
       marginTop: 100
     },
     input: {
-      borderColor: '#f0f',
+      alignSelf: "center",
+      backgroundColor: 'rgb(247, 246, 246)',
+      borderColor: 'rgb(242, 241, 241)',
       borderWidth: 1,
+      marginTop: 10,
+      height: 50,
+      width: 320,
+      borderRadius: 4
+    },
+    inputs: {
+      marginTop: 75  
+    },
+    button: {
+      alignSelf: "center",
       marginTop: 20,
-      height: 50
+      width: 320
+    },
+    footer: {
+      marginTop: 220,
+      alignSelf: "center"
     }
 });
