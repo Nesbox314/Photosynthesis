243da592b53df5a4b110fafd989a86056734f81f
Nesbox314 <ricardopop96@gmail.com>
2020-04-10 23:30:08 -0300

    Arrumando a ordem das telas.

--- Photosynthesis/App.js
+++ Photosynthesis/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import Login from "./src/pages/nova_senha";
+import Login from "./src/pages/login";
 
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import nova_senha from './src/pages/cadastro_de_plantas';
-import cadastro_de_plantas from './src/pages/cadastro_de_plantas';
 
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
-    <Stack.Navigator screenOptions={{
-      headerShown: false
-    }}>
-      <Stack.Screen name="cadastro_de_plantas" component={cadastro_de_plantas} />
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="login" component={Login} />
     </Stack.Navigator>
   );
 }
