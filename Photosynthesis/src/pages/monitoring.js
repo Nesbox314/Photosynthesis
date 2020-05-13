d39af0ca1838437ea012b96b2c9f804f4fc87547
leonardo-kons <49040747+leonardo-kons@users.noreply.github.com>
2020-05-12 22:21:12 -0300

    estruturação e estilização da tela de meus monitoramentos
    Estruturação e estilização da tela de seleção dos monitoramentos.

--- Photosynthesis/src/pages/monitoring.js
+++ Photosynthesis/src/pages/monitoring.js
@@ -1,11 +1,6 @@
-
-
-
-
-
 import React from 'react';
 import { Component } from "react";
-import { Text, Image, StyleSheet, View, Button, TextInput, Alert } from "react-native";
+import { Text, Image, StyleSheet, View, Button, TextInput, Alert, ScrollView , TouchableOpacity } from "react-native";
 import Header from './component/header';
 import TabNavigator from './component/tabNavigator';
 
@@ -17,75 +12,85 @@ export default class Monitoring extends Component {
                 <View>
                     <Header navigation={this.props.navigation}/>
                 </View>
-                <View >
-                    <Text style={styles.titulo} >Meus monitoramentos</Text>
+                <View style={styles.tituloPrincipalContainer}>
+                    <Text style={styles.tituloPrincipal} >Meus monitoramentos</Text>
                 </View>
-                <View style={styles.imagens}>
-                    <Image source={require('../../assets/girassol.jpg')} style={styles.imagem}></Image>
-                    <Text style={styles.tituloPlantas} >planta 1</Text>
-                    <Image source={require('../../assets/girassol.jpg')} style={styles.imagem}></Image>
-                </View>
+                <View style={styles.aa}>
+                    <ScrollView style={styles.scrollView}>
+                        <TouchableOpacity activeOpacity={.100} style={styles.monitoramento}  onPress={() => this.props.navigation.navigate('solicitar_n_senha')}>
+                            <View style={styles.tituloPlantasContainer}>
+                                <Text style={styles.tituloPlantas} >Helianthus annus</Text>
+                                <Text style={styles.tituloPlantas} >girassol</Text>
+                            </View>  
+                            <Image source={require('../../assets/girassol.jpg')} style={styles.imagem}></Image>
+                        </TouchableOpacity>
                 
+                        <TouchableOpacity activeOpacity={.100} style={styles.monitoramento}  onPress={() => this.props.navigation.navigate('solicitar_n_senha')} style={styles.monitoramento}>
+                            <View style={styles.tituloPlantasContainer}>
+                                <Text style={styles.tituloPlantas} >Helianthus annus</Text>
+                                <Text style={styles.tituloPlantas} >girassol</Text>
+                            </View>  
+                            <Image source={require('../../assets/girassol.jpg')} style={styles.imagem}></Image>
+                        </TouchableOpacity>
+                    </ScrollView>
+                    </View>
                 <View style={styles.footer}>
                     <TabNavigator style={styles.tabNavigator} navigation={this.props.navigation}/>
                 </View>
-            </View>
+            </View>   
         )
     }
 }
 
 const styles = StyleSheet.create({
-    logosu:{
-        height:100
+   
+    tituloPrincipalContainer:{
+        borderBottomWidth: 1,
+        borderBottomColor: '#DCDCDC',
     },
-    nivel:{
-        marginLeft:70,
-        marginTop:-30,
-        fontSize: 20
-    },
-    resposta:{
-        marginLeft:200,
-        marginTop:-25,
-        fontSize: 20
-    },
-    L1: {
-        marginTop: 5
-    },
-    L2: {
-    },
-    icon:{
-        width: 50,
-        height: 50,
-        marginTop: 10
-    },
-    icons: {
-        marginTop: 20,
-        alignSelf: "center"
-    },
-    titulo:{
+    tituloPrincipal:{
         marginTop: 90,
+        paddingBottom:15,
         fontSize: 20,
-        alignSelf: "center"
+        alignSelf: "center",
+        fontWeight: "bold"
+     
+    },
+    tituloPlantasContainer:{
+        marginTop:20
     },
     tituloPlantas:{
-        
+        alignSelf: "center",
+        fontSize: 17,
     },
     imagem: {
-        marginTop: 30,
         height: 200,
-        width: 400,
+        width: 450,
         alignSelf: "center",
+        marginBottom: 8,
+        marginTop:10
     },
-    imagens: {
-        marginTop: -10,
+    monitoramento: {
         
     },
     footer: {
-       
-        bottom:0
+        marginTop:-14,
+         
     },
+    
+    scrollView:{
+        marginHorizontal: 0,
+        marginBottom:0,
+      
+    },
+    
     tabNavigator: {
-        bottom: 0
-    }
+        bottom: 0,
+        marginTop: 0
+    },
+
+    aa:{
+        height:500,
+    },
 })
 
